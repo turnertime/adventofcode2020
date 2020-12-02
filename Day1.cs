02f27d0948b3f524b413e2ec8d8cabb0d52744b7
Drew Turner <turnertime@outlook.com>
2020-12-01 21:25:02 -0700

    Solve Day 1 and Part II

--- Day1.cs
+++ Day1.cs
@@ -1,11 +1,46 @@
+using System.Linq;
+
 internal static class Day1
 {
 
     /// <summary>
     /// <a href="https://adventofcode.com/2020/day/1">Day 1</a>: Report Repair
     /// </summary>
-    public static string Run(string input) {
-        return $"'{input}'";
+    public static string Run(string[] input)
+    {
+        var numbers = input.Select(int.Parse).ToArray();
+        for (var i = 0; i < numbers.Length; i++)
+        {
+            for (var j = 0; j < numbers.Length; j++)
+            {
+                if (numbers[i] + numbers[j] == 2020)
+                {
+                    return $"{numbers[i] * numbers[j]}";
+                }
+            }
+        }
+        System.Console.Beep();
+        return "Inconclusive!";
+    }
+
+    public static string RunPartII(string[] input)
+    {
+        var numbers = input.Select(int.Parse).ToArray();
+        for (var i = 0; i < numbers.Length; i++)
+        {
+            for (var j = 1; j < numbers.Length; j++)
+            {
+                for (var k = 0; k < numbers.Length; k++)
+                {
+                    if (numbers[i] + numbers[j] + numbers[k] == 2020)
+                    {
+                        return $"{numbers[i] * numbers[j] *  numbers[k]}";
+                    }
+                }
+            }
+        }
+        System.Console.Beep();
+        return "Inconclusive!";
     }
 
 }
--- Program.cs
+++ Program.cs
@@ -6,14 +6,19 @@ var day = args[0];
 var binDirectory = System.AppDomain.CurrentDomain.BaseDirectory;
 var path = System.IO.Path.Combine(binDirectory, $"../../../Day{day}.txt");
 var input = System.IO.File.Exists(path)
-    ? System.IO.File.ReadAllText(path)
-    : string.Empty;
+    ? System.IO.File.ReadAllLines(path)
+    : new string[0];
 
 var stopwatch = System.Diagnostics.Stopwatch.StartNew();
 switch (day) {
     case "1":
         Console.WriteLine("--- Day 1: Report Repair ---");
         Console.WriteLine(Day1.Run(input));
+        Console.WriteLine($"Finished in ({stopwatch.ElapsedMilliseconds}) ms");
+        break;
+    case "1-2":
+        Console.WriteLine("--- Day 1: Part Two ---");
+        Console.WriteLine(Day1.RunPartII(input));
         Console.WriteLine($"Finished in ({stopwatch.ElapsedMilliseconds}) ms");
         break;
     default:
