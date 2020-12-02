0252932f9018093a277f37069e1429e15a234318
Drew Turner <turnertime@outlook.com>
2020-12-01 20:08:05 -0700

    Add basic parsing for Day1

---
+++ Day1.cs
@@ -0,0 +1,11 @@
+internal static class Day1
+{
+
+    /// <summary>
+    /// <a href="https://adventofcode.com/2020/day/1">Day 1</a>: Report Repair
+    /// </summary>
+    public static string Run(string input) {
+        return $"'{input}'";
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 
-namespace adventofcode2020
-{
-    public static class Program
-    {
-        public static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+var day = args[0];
+
+// read input if available
+var binDirectory = System.AppDomain.CurrentDomain.BaseDirectory;
+var path = System.IO.Path.Combine(binDirectory, $"../../../Day{day}.txt");
+var input = System.IO.File.Exists(path)
+    ? System.IO.File.ReadAllText(path)
+    : string.Empty;
+
+var stopwatch = System.Diagnostics.Stopwatch.StartNew();
+switch (day) {
+    case "1":
+        Console.WriteLine("--- Day 1: Report Repair ---");
+        Console.WriteLine(Day1.Run(input));
+        Console.WriteLine($"Finished in ({stopwatch.ElapsedMilliseconds}) ms");
+        break;
+    default:
+        Console.Beep();
+        Console.WriteLine($"Day '{day}', was not found.", Console.Error);
+        break;
 }
