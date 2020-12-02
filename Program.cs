742caee29940467ed72f42e73880221cc76964e0
Drew Turner <turnertime@outlook.com>
2020-12-01 19:44:40 -0700

    Add initial advent of code project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace adventofcode2020
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
