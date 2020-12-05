b597094e88c4d0f3552ff89da1733ea390bfe060
Drew Turner <turnertime@outlook.com>
2020-12-04 17:45:11 -0700

    Add System.CommandLine
     - configure basic root command

--- Program.cs
+++ Program.cs
@@ -1,60 +1,80 @@
 ﻿using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
 using System.Diagnostics;
 using System.IO;
 
-// validate day
-var rawDay = args.Length < 1 ? string.Empty : args[0];
-if (!int.TryParse(rawDay, out var day) || day < 1 || day > 25)
+var rootCommand = new RootCommand
 {
-    Console.ForegroundColor = ConsoleColor.Red;
-    Console.WriteLine("Specified day must be between one and twenty five");
-    Console.ResetColor();
-    return;
-}
+    new Argument<int>("day", "Indicates 25 days of christmas") { Arity = ArgumentArity.ExactlyOne },
+    new Option<string>(
+        aliases: new [] { "--dir", "-d" },
+        getDefaultValue: () => Path.Combine(Environment.CurrentDirectory, "input/"),
+        description: "Specifies the directory containing the input")
+};
 
-// read input if available
-var binDirectory = AppDomain.CurrentDomain.BaseDirectory;
-var path = Path.Combine(binDirectory, $"../../../input/Day{day:00}.txt");
-if (!File.Exists(path))
-{
-    Console.ForegroundColor = ConsoleColor.Red;
-    Console.WriteLine($"Day '{rawDay}' input not found");
-    Console.ResetColor();
-    return;
-}
-var input = File.Exists(path) ? File.ReadAllLines(path) : new string[0];
+rootCommand.Description = "Solve Advent of Code 2020 Problem";
 
-// solve the specified day
-var stopwatch = Stopwatch.StartNew();
-Solution solution;
-switch (day)
+rootCommand.Handler = CommandHandler.Create<int, string>((day, dir) =>
 {
-    case 1:
-        Console.WriteLine("--- Day 1: Report Repair ---");
-        solution = Day01.Run(input);
-        break;
-    case 2:
-        Console.WriteLine("--- Day 2: Password Philosophy ---");
-        solution = Day02.Run(input);
-        break;
-    case 3:
-        Console.WriteLine("--- Day 3: Toboggan Trajectory ---");
-        solution = Day03.Run(input);
-        break;
-    case 4:
-        Console.WriteLine("--- Day 4: Passport Processing ---");
-        solution = Day04.Run(input);
-        break;
-    default:
+    // validate day
+    if (day < 1 || day > 25)
+    {
         Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine($"Day '{rawDay}', has not been solved yet.");
+        Console.WriteLine("Specified day must be between one and twenty five");
         Console.ResetColor();
-        return;
-}
+        return 87;
+    }
 
-// write solution to console
-Console.WriteLine($" - A: {solution.PartA}");
-Console.WriteLine($" - B: {solution.PartB}");
-Console.ForegroundColor = ConsoleColor.DarkGreen;
-Console.WriteLine($"--- Finished in ({stopwatch.ElapsedMilliseconds}) ms ---");
-Console.ResetColor();
+    // read input if available
+    var path = Path.Combine(dir, $"Day{day:00}.txt");
+    if (!File.Exists(path))
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"Day '{day}' input not found");
+        Console.ResetColor();
+        return 2;
+    }
+    var input = File.ReadAllLines(path);
+
+    // solve the specified day
+    var stopwatch = Stopwatch.StartNew();
+    Solution solution;
+    switch (day)
+    {
+        case 1:
+            Console.WriteLine("--- Day 1: Report Repair ---");
+            solution = Day01.Run(input);
+            break;
+        case 2:
+            Console.WriteLine("--- Day 2: Password Philosophy ---");
+            solution = Day02.Run(input);
+            break;
+        case 3:
+            Console.WriteLine("--- Day 3: Toboggan Trajectory ---");
+            solution = Day03.Run(input);
+            break;
+        case 4:
+            Console.WriteLine("--- Day 4: Passport Processing ---");
+            solution = Day04.Run(input);
+            break;
+        default:
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Day '{day}', has not been solved yet.");
+            Console.ResetColor();
+            return 87;
+    }
+
+    // write solution to console
+    Console.WriteLine($" - A: {solution.PartA}");
+    Console.WriteLine($" - B: {solution.PartB}");
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.WriteLine($"--- Finished in ({stopwatch.ElapsedMilliseconds}) ms ---");
+    Console.ResetColor();
+    return 0;
+
+});
+
+return rootCommand.InvokeAsync(args).Result;
+
+
